97bde6a98db5fa4720bbe8ce6a74e59cddb6f7a9
Rumaiha <rumaihashafiq@hotmail.com>
2025-07-14 13:41:17 +0500

    Initial commit

--- Backend/utils/ageValidator.js
+++
@@ -1,4 +0,0 @@
-module.exports = function validateAge(dob) {
-  const age = Math.floor((Date.now() - new Date(dob)) / (365.25 * 24 * 60 * 60 * 1000));
-  return age > 15;
-};
--- Backend/utils/validatePassword.js
+++
@@ -1,4 +0,0 @@
-module.exports = function validatePassword(password) {
-  const regex = /^[A-Z](?=.*[a-z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{7,}$/;
-  return regex.test(password);
-};
